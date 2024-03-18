bc0f932037116498bd916e5cc52cb1bc0eca3dc4
Dimitri <dzmitry.brusilouski@hs-augsburg.de>
2024-03-18 18:20:52 +0100

    modified: adjusted global list for usetypes-strings

--- Script_Animation.cpp
+++ Script_Animation.cpp
@@ -7,127 +7,6 @@
 #include "Script.h"
 
 
-
-static void InitMap ( ) {
-    useTypeMap[gEUseType_None] = "None";
-    useTypeMap[gEUseType_Action] = "Action";
-    useTypeMap[gEUseType_1H] = "1H";
-    useTypeMap[gEUseType_2H] = "2H";
-    useTypeMap[gEUseType_Arrow] = "Arrow";
-    useTypeMap[gEUseType_Bow] = "Bow";
-    useTypeMap[gEUseType_CrossBow] = "CrossBow";
-    useTypeMap[gEUseType_Bolt] = "Bolt";
-    useTypeMap[gEUseType_Fist] = "Fist";
-    useTypeMap[gEUseType_Shield] = "Shield";
-    useTypeMap[gEUseType_Armor] = "Armor";
-    useTypeMap[gEUseType_Helmet] = "Helmet";
-    useTypeMap[gEUseType_Staff] = "Staff";
-    useTypeMap[gEUseType_Amulet] = "Amulet";
-    useTypeMap[gEUseType_Ring] = "Ring";
-    useTypeMap[gEUseType_Cast] = "Cast";
-    useTypeMap[gEUseType_Potion] = "Potion";
-    useTypeMap[gEUseType_Plant] = "Bread";
-    useTypeMap[gEUseType_Meat] = "Meat";
-    useTypeMap[gEUseType_Fruit] = "Fruit";
-    useTypeMap[gEUseType_Bread] = "Bread";
-    useTypeMap[gEUseType_Bottle] = "Potion";
-    useTypeMap[gEUseType_Cup] = "Cup";
-    useTypeMap[gEUseType_Bowl] = "Bowl";
-    useTypeMap[gEUseType_Torch] = "Torch";
-    useTypeMap[gEUseType_Alarmhorn] = "Alarmhorn";
-    useTypeMap[gEUseType_Broom] = "Staff";
-    useTypeMap[gEUseType_Brush] = "Block";
-    useTypeMap[gEUseType_Lute] = "Lute";
-    useTypeMap[gEUseType_Rake] = "Staff";
-    useTypeMap[gEUseType_TrophyTeeth] = "TrophyTeeth";
-    useTypeMap[gEUseType_Valuable] = "Valuable";
-    useTypeMap[gEUseType_Smoke] = "Smoke";
-    useTypeMap[gEUseType_OrcPipe] = "OrcPipe";
-    useTypeMap[gEUseType_Scoop] = "Tool";
-    useTypeMap[gEUseType_Stick] = "Tool";
-    useTypeMap[gEUseType_Shovel] = "Staff";
-    useTypeMap[gEUseType_Hammer] = "Tool";
-    useTypeMap[gEUseType_Fan] = "Staff";
-    useTypeMap[gEUseType_Pan] = "Tool";
-    useTypeMap[gEUseType_Saw] = "Tool";
-    useTypeMap[gEUseType_TrophySkin] = "TrophySkin";
-    useTypeMap[gEUseType_Map] = "Letter";
-    useTypeMap[gEUseType_Book] = "Letter";
-    useTypeMap[gEUseType_Letter] = "Letter";
-    useTypeMap[gEUseType_Key] = "Key";
-    useTypeMap[gEUseType_Lockpick] = "Key";
-    useTypeMap[gEUseType_CarryFront] = "CarryFront";
-    useTypeMap[gEUseType_CarryShoulder] = "CarryShoulder";
-    useTypeMap[gEUseType_Pickaxe] = "Axe";
-    useTypeMap[gEUseType_TrophyFur] = "TrophyFur";
-    useTypeMap[gEUseType_Halberd] = "Staff";
-    useTypeMap[gEUseType_Axe] = "Axe";
-    useTypeMap[gEUseType_ITEM_E] = "ITEM_E";
-    useTypeMap[gEUseType_Modify] = "Modify";
-    useTypeMap[gEUseType_PhysicalFist] = "Fist";
-    useTypeMap[gEUseType_ITEM_H] = "ITEM_H";
-    useTypeMap[gEUseType_Anvil] = "Anvil";
-    useTypeMap[gEUseType_Forge] = "Forge";
-    useTypeMap[gEUseType_GrindStone] = "GrindStone";
-    useTypeMap[gEUseType_Cauldron] = "Cauldron";
-    useTypeMap[gEUseType_Barbecue] = "Barbecue";
-    useTypeMap[gEUseType_Alchemy] = "Alchemy";
-    useTypeMap[gEUseType_Bookshelf] = "Bookshelf";
-    useTypeMap[gEUseType_Bookstand] = "Bookstand";
-    useTypeMap[gEUseType_TakeStone] = "TakeStone";
-    useTypeMap[gEUseType_DropStone] = "DropStone";
-    useTypeMap[gEUseType_PickOre] = "PickOre";
-    useTypeMap[gEUseType_PickGround] = "PickGround";
-    useTypeMap[gEUseType_DigGround] = "DigGround";
-    useTypeMap[gEUseType_Field] = "Field";
-    useTypeMap[gEUseType_Repair] = "Repair";
-    useTypeMap[gEUseType_SawLog] = "SawLog";
-    useTypeMap[gEUseType_Lumberjack] = "Lumberjack";
-    useTypeMap[gEUseType_Bed] = "Bed";
-    useTypeMap[gEUseType_SleepGround] = "SleepGround";
-    useTypeMap[gEUseType_CleanFloor] = "CleanFloor";
-    useTypeMap[gEUseType_Dance] = "Dance";
-    useTypeMap[gEUseType_FanBoss] = "FanBoss";
-    useTypeMap[gEUseType_Boss] = "Boss";
-    useTypeMap[gEUseType_Throne] = "Throne";
-    useTypeMap[gEUseType_Pace] = "Pace";
-    useTypeMap[gEUseType_Bard] = "Bard";
-    useTypeMap[gEUseType_Stool] = "Stool";
-    useTypeMap[gEUseType_Bench] = "Bench";
-    useTypeMap[gEUseType_Waterpipe] = "Waterpipe";
-    useTypeMap[gEUseType_WaterBarrel] = "WaterBarrel";
-    useTypeMap[gEUseType_PirateTreasure] = "Stove";
-    useTypeMap[gEUseType_Campfire] = "Campfire";
-    useTypeMap[gEUseType_SitCampfire] = "SitCampfire";
-    useTypeMap[gEUseType_SitGround] = "SitGround";
-    useTypeMap[gEUseType_Smalltalk] = "Smalltalk";
-    useTypeMap[gEUseType_Preach] = "Preach";
-    useTypeMap[gEUseType_Spectator] = "Spectator";
-    useTypeMap[gEUseType_Stand] = "Stand";
-    useTypeMap[gEUseType_Guard] = "Guard";
-    useTypeMap[gEUseType_Trader] = "Trader";
-    useTypeMap[gEUseType_Listener] = "Listener";
-    useTypeMap[gEUseType_OrcDance] = "OrcDance";
-    useTypeMap[gEUseType_Stoneplate] = "Stoneplate";
-    useTypeMap[gEUseType_OrcDrum] = "OrcDrum";
-    useTypeMap[gEUseType_Door] = "Door";
-    useTypeMap[gEUseType_OrcBoulder] = "OrcBoulder";
-    useTypeMap[gEUseType_EatGround] = "EatGround";
-    useTypeMap[gEUseType_DrinkWater] = "DrinkWater";
-    useTypeMap[gEUseType_Pee] = "Pee";
-    useTypeMap[gEUseType_Chest] = "Chest";
-    useTypeMap[gEUseType_Shrine] = "Shrine";
-    useTypeMap[gEUseType_AttackPoint] = "AttackPoint";
-    useTypeMap[gEUseType_Roam] = "Roam";
-    useTypeMap[gEUseType_BODY_A] = "BODY_A";
-    useTypeMap[gEUseType_Beard] = "Beard";
-    useTypeMap[gEUseType_Hair] = "Hair";
-    useTypeMap[gEUseType_Head] = "Head";
-    useTypeMap[gEUseType_Body] = "Body";
-    useTypeMap[gEUseType_Flee] = "Flee";
-    useTypeMap[gEUseType_Talk] = "Talk";
-}
-
 gSScriptInit & GetScriptInit()
 {
     static gSScriptInit s_ScriptInit;
@@ -180,7 +59,7 @@ void GE_STDCALL GetAniName ( bCString& p_RetString, eCEntity* p_Entity, gEAction
         }
         else {
             if ( leftWeapon.Interaction.IsValid ( ) ) {
-                p_RetString += useTypeMap[leftWeapon.Interaction.GetProperty<PSInteraction::PropertyUseType> ( )];
+                p_RetString += *( char** )( RVA_Game ( 0x2bac98 ) + static_cast< GEInt >( leftWeapon.Interaction.GetProperty<PSInteraction::PropertyUseType> ( ) )* 0x4);
                 p_RetString += "_";
                 //std::cout << "RetString: " << p_RetString << "\n";
             }
@@ -198,7 +77,7 @@ void GE_STDCALL GetAniName ( bCString& p_RetString, eCEntity* p_Entity, gEAction
         }
         else {
             if ( rightWeapon.Interaction.IsValid ( ) ) {
-                p_RetString += useTypeMap[rightWeapon.Interaction.GetProperty<PSInteraction::PropertyUseType> ( )];
+                p_RetString += *( char** )( RVA_Game ( 0x2bac98 ) + static_cast< GEInt >( rightWeapon.Interaction.GetProperty<PSInteraction::PropertyUseType> ( ) ) * 0x4 );
                 p_RetString += "_";
                 //std::cout << "RetString: " << p_RetString << "\n";
             }
@@ -240,6 +119,20 @@ void GE_STDCALL GetAniName ( bCString& p_RetString, eCEntity* p_Entity, gEAction
     //std::cout << "RetString: " << p_RetString << "\n";
 }
 
+/**
+* Adjust the List of bCString ID-Values for the Usetype (117 gEUseType)
+* With that all Game.dll AnimationName Choosers use the Namespace of the Weapontype
+* It is specifically needed for more seperation of the Games 2H-Animations which are Shared between 2H, Axes, Pickaxes, and Halberts
+*/
+void AdjustUseTypeStringList ( ) {
+    DWORD currProt , newProt;
+    VirtualProtect ( ( LPVOID )RVA_Game ( 0x2bac98 ) , 117*0x4 , PAGE_EXECUTE_READWRITE , &currProt );
+    memset ( ( LPVOID )RVA_Game ( 0x2bac98 ) , 0x90 , 117 * 0x4 );
+    memcpy ( ( LPVOID )RVA_Game ( 0x2bac98 ) , useTypesStrings , 117 * 0x4 );
+    VirtualProtect ( ( LPVOID )RVA_Game ( 0x2bac98 ) , 117 * 0x4 , currProt , &newProt );
+}
+
+
 extern "C" __declspec( dllexport )
 gSScriptInit const * GE_STDCALL ScriptInit( void )
 {
@@ -247,7 +140,8 @@ gSScriptInit const * GE_STDCALL ScriptInit( void )
     //freopen_s ( ( FILE** )stdout , "CONOUT$" , "w" , stdout );
     // Ensure that that Script_Game.dll is loaded.
     GetScriptAdmin().LoadScriptDLL("Game.dll");
-    InitMap ( );
+    //InitMap ( );
+    AdjustUseTypeStringList ( );
     static mCFunctionHook Hook_GetAniName;
 
     Hook_GetAniName.Hook ( RVA_Game ( 0x16f840 ) , &GetAniName , mCBaseHook::mEHookType_ThisCall );
--- Script_Animation.h
+++ Script_Animation.h
@@ -1,9 +1,245 @@
 #pragma once
 
 #include "Script.h"
-#include <map>
+//#include <map>
 
 gSScriptInit & GetScriptInit();
-
-static std::map<gEUseType , bCString> useTypeMap;
-static void InitMap ( );
+static const char* useTypesStrings[] = {
+ "None" ,
+ "Action",
+ "1H",
+ "2H",
+ "Arrow",
+ "Bow",
+ "CrossBow",
+ "Bolt",
+ "Fist",
+ "Shield",
+ "Armor",
+ "Helmet",
+ "Staff",
+ "Amulet",
+ "Ring",
+ "Cast",
+ "Potion",
+ "Bread",
+ "Meat",
+ "Fruit",
+ "Bread",
+ "Potion",
+ "Cup",
+ "Bowl",
+ "Torch",
+ "Alarmhorn",
+ "Staff",
+ "Block",
+ "Lute",
+ "Staff",
+ "TrophyTeeth",
+ "Valuable",
+ "Smoke",
+ "OrcPipe",
+ "Tool",
+ "Tool",
+ "Staff",
+ "Tool",
+ "Staff",
+ "Tool",
+ "Tool",
+ "TrophySkin",
+ "Letter",
+ "Letter",
+ "Letter",
+ "Key",
+ "Key",
+ "CarryFront",
+ "CarryShoulder",
+ "Axe",
+ "TrophyFur",
+ "Staff",
+ "Axe",
+ "ITEM_E",
+ "Modify",
+ "Fist",
+ "ITEM_H",
+ "Anvil",
+ "Forge",
+ "GrindStone",
+ "Cauldron",
+ "Barbecue",
+ "Alchemy",
+ "Bookshelf",
+ "Bookstand",
+ "TakeStone",
+ "DropStone",
+ "PickOre",
+ "PickGround",
+ "DigGround",
+ "Field",
+ "Repair",
+ "SawLog",
+ "Lumberjack",
+ "Bed",
+ "SleepGround",
+ "CleanFloor",
+ "Dance",
+ "FanBoss",
+ "Boss",
+ "Throne",
+ "Pace",
+ "Bard",
+ "Stool",
+ "Bench",
+ "Waterpipe",
+ "WaterBarrel",
+ "Stove",
+ "Campfire",
+ "SitCampfire",
+ "SitGround",
+ "Smalltalk",
+ "Preach",
+ "Spectator",
+ "Stand",
+ "Guard",
+ "Trader",
+ "Listener",
+ "OrcDance",
+ "Stoneplate",
+ "OrcDrum",
+ "Door",
+ "OrcBoulder",
+ "EatGround",
+ "DrinkWater",
+ "Pee",
+ "Chest",
+ "Shrine",
+ "AttackPoint",
+ "Roam",
+ "BODY_A",
+ "Beard",
+ "Hair",
+ "Head",
+ "Body",
+ "Flee",
+ "Talk" ,
+};
+//static std::map<gEUseType , bCString> useTypeMap;
+/*static void InitMap ( ) {
+    useTypeMap[gEUseType_None] = "None";
+    useTypeMap[gEUseType_Action] = "Action";
+    useTypeMap[gEUseType_1H] = "1H";
+    useTypeMap[gEUseType_2H] = "2H";
+    useTypeMap[gEUseType_Arrow] = "Arrow";
+    useTypeMap[gEUseType_Bow] = "Bow";
+    useTypeMap[gEUseType_CrossBow] = "CrossBow";
+    useTypeMap[gEUseType_Bolt] = "Bolt";
+    useTypeMap[gEUseType_Fist] = "Fist";
+    useTypeMap[gEUseType_Shield] = "Shield";
+    useTypeMap[gEUseType_Armor] = "Armor";
+    useTypeMap[gEUseType_Helmet] = "Helmet";
+    useTypeMap[gEUseType_Staff] = "Staff";
+    useTypeMap[gEUseType_Amulet] = "Amulet";
+    useTypeMap[gEUseType_Ring] = "Ring";
+    useTypeMap[gEUseType_Cast] = "Cast";
+    useTypeMap[gEUseType_Potion] = "Potion";
+    useTypeMap[gEUseType_Plant] = "Bread";
+    useTypeMap[gEUseType_Meat] = "Meat";
+    useTypeMap[gEUseType_Fruit] = "Fruit";
+    useTypeMap[gEUseType_Bread] = "Bread";
+    useTypeMap[gEUseType_Bottle] = "Potion";
+    useTypeMap[gEUseType_Cup] = "Cup";
+    useTypeMap[gEUseType_Bowl] = "Bowl";
+    useTypeMap[gEUseType_Torch] = "Torch";
+    useTypeMap[gEUseType_Alarmhorn] = "Alarmhorn";
+    useTypeMap[gEUseType_Broom] = "Staff";
+    useTypeMap[gEUseType_Brush] = "Block";
+    useTypeMap[gEUseType_Lute] = "Lute";
+    useTypeMap[gEUseType_Rake] = "Staff";
+    useTypeMap[gEUseType_TrophyTeeth] = "TrophyTeeth";
+    useTypeMap[gEUseType_Valuable] = "Valuable";
+    useTypeMap[gEUseType_Smoke] = "Smoke";
+    useTypeMap[gEUseType_OrcPipe] = "OrcPipe";
+    useTypeMap[gEUseType_Scoop] = "Tool";
+    useTypeMap[gEUseType_Stick] = "Tool";
+    useTypeMap[gEUseType_Shovel] = "Staff";
+    useTypeMap[gEUseType_Hammer] = "Tool";
+    useTypeMap[gEUseType_Fan] = "Staff";
+    useTypeMap[gEUseType_Pan] = "Tool";
+    useTypeMap[gEUseType_Saw] = "Tool";
+    useTypeMap[gEUseType_TrophySkin] = "TrophySkin";
+    useTypeMap[gEUseType_Map] = "Letter";
+    useTypeMap[gEUseType_Book] = "Letter";
+    useTypeMap[gEUseType_Letter] = "Letter";
+    useTypeMap[gEUseType_Key] = "Key";
+    useTypeMap[gEUseType_Lockpick] = "Key";
+    useTypeMap[gEUseType_CarryFront] = "CarryFront";
+    useTypeMap[gEUseType_CarryShoulder] = "CarryShoulder";
+    useTypeMap[gEUseType_Pickaxe] = "Axe";
+    useTypeMap[gEUseType_TrophyFur] = "TrophyFur";
+    useTypeMap[gEUseType_Halberd] = "Staff";
+    useTypeMap[gEUseType_Axe] = "Axe";
+    useTypeMap[gEUseType_ITEM_E] = "ITEM_E";
+    useTypeMap[gEUseType_Modify] = "Modify";
+    useTypeMap[gEUseType_PhysicalFist] = "Fist";
+    useTypeMap[gEUseType_ITEM_H] = "ITEM_H";
+    useTypeMap[gEUseType_Anvil] = "Anvil";
+    useTypeMap[gEUseType_Forge] = "Forge";
+    useTypeMap[gEUseType_GrindStone] = "GrindStone";
+    useTypeMap[gEUseType_Cauldron] = "Cauldron";
+    useTypeMap[gEUseType_Barbecue] = "Barbecue";
+    useTypeMap[gEUseType_Alchemy] = "Alchemy";
+    useTypeMap[gEUseType_Bookshelf] = "Bookshelf";
+    useTypeMap[gEUseType_Bookstand] = "Bookstand";
+    useTypeMap[gEUseType_TakeStone] = "TakeStone";
+    useTypeMap[gEUseType_DropStone] = "DropStone";
+    useTypeMap[gEUseType_PickOre] = "PickOre";
+    useTypeMap[gEUseType_PickGround] = "PickGround";
+    useTypeMap[gEUseType_DigGround] = "DigGround";
+    useTypeMap[gEUseType_Field] = "Field";
+    useTypeMap[gEUseType_Repair] = "Repair";
+    useTypeMap[gEUseType_SawLog] = "SawLog";
+    useTypeMap[gEUseType_Lumberjack] = "Lumberjack";
+    useTypeMap[gEUseType_Bed] = "Bed";
+    useTypeMap[gEUseType_SleepGround] = "SleepGround";
+    useTypeMap[gEUseType_CleanFloor] = "CleanFloor";
+    useTypeMap[gEUseType_Dance] = "Dance";
+    useTypeMap[gEUseType_FanBoss] = "FanBoss";
+    useTypeMap[gEUseType_Boss] = "Boss";
+    useTypeMap[gEUseType_Throne] = "Throne";
+    useTypeMap[gEUseType_Pace] = "Pace";
+    useTypeMap[gEUseType_Bard] = "Bard";
+    useTypeMap[gEUseType_Stool] = "Stool";
+    useTypeMap[gEUseType_Bench] = "Bench";
+    useTypeMap[gEUseType_Waterpipe] = "Waterpipe";
+    useTypeMap[gEUseType_WaterBarrel] = "WaterBarrel";
+    useTypeMap[gEUseType_PirateTreasure] = "Stove";
+    useTypeMap[gEUseType_Campfire] = "Campfire";
+    useTypeMap[gEUseType_SitCampfire] = "SitCampfire";
+    useTypeMap[gEUseType_SitGround] = "SitGround";
+    useTypeMap[gEUseType_Smalltalk] = "Smalltalk";
+    useTypeMap[gEUseType_Preach] = "Preach";
+    useTypeMap[gEUseType_Spectator] = "Spectator";
+    useTypeMap[gEUseType_Stand] = "Stand";
+    useTypeMap[gEUseType_Guard] = "Guard";
+    useTypeMap[gEUseType_Trader] = "Trader";
+    useTypeMap[gEUseType_Listener] = "Listener";
+    useTypeMap[gEUseType_OrcDance] = "OrcDance";
+    useTypeMap[gEUseType_Stoneplate] = "Stoneplate";
+    useTypeMap[gEUseType_OrcDrum] = "OrcDrum";
+    useTypeMap[gEUseType_Door] = "Door";
+    useTypeMap[gEUseType_OrcBoulder] = "OrcBoulder";
+    useTypeMap[gEUseType_EatGround] = "EatGround";
+    useTypeMap[gEUseType_DrinkWater] = "DrinkWater";
+    useTypeMap[gEUseType_Pee] = "Pee";
+    useTypeMap[gEUseType_Chest] = "Chest";
+    useTypeMap[gEUseType_Shrine] = "Shrine";
+    useTypeMap[gEUseType_AttackPoint] = "AttackPoint";
+    useTypeMap[gEUseType_Roam] = "Roam";
+    useTypeMap[gEUseType_BODY_A] = "BODY_A";
+    useTypeMap[gEUseType_Beard] = "Beard";
+    useTypeMap[gEUseType_Hair] = "Hair";
+    useTypeMap[gEUseType_Head] = "Head";
+    useTypeMap[gEUseType_Body] = "Body";
+    useTypeMap[gEUseType_Flee] = "Flee";
+    useTypeMap[gEUseType_Talk] = "Talk";
+}*/
